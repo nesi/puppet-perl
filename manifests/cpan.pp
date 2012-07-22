972759fecde4812c98617993ca0392904776d704
Aaron Hicks (Landcare PN) <hicksa@landcareresearch.co.nz>
2012-07-23 11:17:36 +1200

    needs some paths

--- manifests/cpan.pp
+++ manifests/cpan.pp
@@ -5,8 +5,9 @@ define perl::cpan(
 
 ){
   exec{"cpan_load_${name}":
-   command 	=> "perl -MCPAN -e '\$ENV{PERL_MM_USE_DEFAULT}=1; CPAN::Shell->install(\"${name}\")'",
-   onlyif 	=> "test `perl -M${name} -e 'print 1' 2>/dev/null || echo 0` == '0'",
-   require 	=> [Package[$perl::package],File["/root/.cpan/CPAN/MyConfig.pm"]],
+  	path 		=> ['/usr/bin/','/bin'],
+   	command => "perl -MCPAN -e '\$ENV{PERL_MM_USE_DEFAULT}=1; CPAN::Shell->install(\"${name}\")'",
+   	onlyif 	=> "test `perl -M${name} -e 'print 1' 2>/dev/null || echo 0` == '0'",
+   	require => [Package[$perl::package],File["/root/.cpan/CPAN/MyConfig.pm"]],
   }
 }
