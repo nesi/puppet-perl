f5735ddfae8c4c56d0c72418e99f2391f3dd7871
Aaron Hicks (Landcare PN) <hicksa@landcareresearch.co.nz>
2012-07-23 11:04:29 +1200

    A perl module with cpan configured

---
+++ manifests/init.pp
@@ -0,0 +1,42 @@
+# An installer manifest for perl,
+# It's not complicated but required to 'standardise' the Perl install
+# and make some variables accessible to other parts of this module
+#
+# The default CPAN mirror is in New Zealand, so choose a more suitabled one
+# from http://www.cpan.org/SITES.html
+
+class perl (
+	$version 			= false,
+	$ensure				= installed,
+	$cpan_mirror	= 'ftp://ftp.auckland.ac.nz/pub/perl/CPAN/'
+){
+
+	$package = 'perl'
+
+	if $version == false {
+		package{$package:
+			ensure => $ensure,
+		}
+	} else {
+		package{$package:
+			ensure 	=> $ensure,
+			version => $version
+		}
+	}
+
+	exec{'configure_cpan':
+		command	=> "cpan <<EOF
+yes	
+yes
+no
+no
+${cpan_mirror}
+
+yes
+quit
+EOF",
+		creates => "/root/.cpan/CPAN/MyConfig.pm",
+		require => Package[$package]
+	}
+
+}
