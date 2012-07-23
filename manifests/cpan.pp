0a7e58ab8a817e26f7963874905753be15d7584e
Aaron Hicks (Landcare PN) <hicksa@landcareresearch.co.nz>
2012-07-23 13:12:20 +1200

    using pmvers to test for a CPAN module...

--- manifests/cpan.pp
+++ manifests/cpan.pp
@@ -11,18 +11,18 @@ define perl::cpan(
 	  exec{"cpan_load_${name}":
 	  	path 		=> ['/usr/bin/','/bin'],
 	   	command => "cpan -i ${name}",
-	   	unless 	=> "perl -M${name} -e 'print \"${name} loaded\"'",
+	   	unless 	=> "pmvers ${name}",
 	   	timeout => $timeout,
-	   	require => [Package[$perl::package],Exec['configure_cpan']],
+	   	require => [Package[$perl::package,$perl::pmtools_package],Exec['configure_cpan']],
 	  }
 	} elsif $ensure == absent {
 		if $name != "App::pmuninstall"{
 			exec{"cpan_unload_${name}":
 		  	path 		=> ['/usr/bin/','/bin','/usr/local/bin'],
 		   	command => "pm-uninstall ${name}",
-		   	onlyif 	=> "perl -M${name} -e 'print 1' 2>/dev/null",
+		   	onlyif 	=> "pmvers ${name}",
 		   	timeout => $timeout,
-		   	require => [Package[$perl::package],Exec['configure_cpan','install_pmuninstall']],
+		   	require => [Package[$perl::package,$perl::pmtools_package],Exec['configure_cpan','install_pmuninstall']],
 		  }
 		} else {
 			warning("App::pmuninstall is required, and will not be uninstalled on ${fqdn}")
